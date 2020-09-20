146091e9754447d4641231aca96ec75177d6ecc1
CornanZZ <63889937+CornanZZ@users.noreply.github.com>
2020-09-20 17:55:34 +0800

    Add files via upload

--- Notes/ZhangZhao/Code/test3.cpp
+++ Notes/ZhangZhao/Code/test3.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
-#include <vector>
-#include <unordered_set>
+//#include <vector>
+//#include <unordered_set>
 //#include <string>
 //#include <algorithm>
 using namespace std;
 
+#define maxn 33000
+int pre[maxn], num[maxn];	//i的根结点为pre[i], i的子图中包含num[i]个节点。 
+int a[maxn];
+void init(int n){
+	for(int i = 0; i < n; ++i){
+		pre[i] = i;
+		num[i] = 1;
+	}
+}
+int find(int x){
+	if(x == pre[x])
+		return x;
+	
+	pre[x] = find(pre[x]);	//递归地查找当前连通图的根节点 
+	return pre[x];
+}
+void join(int x, int y){
+	int px = find(x), py = find(y);
+	if(px != py){
+		pre[px] = py;	//连接两个子图 
+		num[py] += num[px];	//汇总两个子图的节点 
+	}
+}
+
 int main(){
-	int n, m;	
-	scanf("%d %d", &n, &m);	
-	while(m | n){
-		unordered_set<int> s;
-		s.insert(0);
+	int n, m;		
+	while(scanf("%d %d", &n, &m), m || n){
+		init(n);
 		for(int i = 0; i < m; ++i){
 			int k;	
 			scanf("%d", &k);
-			vector<int> num;
-			bool flag = false;
-			int tmp;
-			for(int j = 0; j < k; ++j){				
-				scanf("%d", &tmp);
-				num.push_back(tmp);	
-				if(!flag && s.find(tmp) != s.end()) flag = true;
-			}
-			if(flag){
-				for(int j = 0; j < k; ++j)
-					s.insert(num[j]);	
-			}
-			if(s.size() == n) break;
+			for(int j = 0; j < k; ++j)			
+				scanf("%d", &a[j]);
+			for(int j = 0; j < k - 1; ++j)			
+				join(a[j], a[j + 1]);			
 		}
-		printf("%d\n", s.size());	
-			
-		scanf("%d %d", &n, &m);	
+		int root = find(0);
+		printf("%d\n", num[root]);		
 	}
 	return 0;
 }
